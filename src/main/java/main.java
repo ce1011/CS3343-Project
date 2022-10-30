e5888cdb63236444201c4d9488a97f5adf31c2b3
ce1011 <paco20071001@gmail.com>
2022-10-31 07:26:58 +0800

    Fix git message

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,11 +1,7 @@
-<<<<<<< Updated upstream
-public class main {
-=======
 
 class Main{
     public static void main(String[] args) {
         User user = new User("user","user",new Customer());
         System.out.print(user.getUsername);
     }
->>>>>>> Stashed changes
 }
