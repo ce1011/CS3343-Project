5ba3d232a78836a13638c1009b3a5d42cee45216
ivanlong20 <82087413+ivanlong20@users.noreply.github.com>
2022-12-01 14:03:35 +0800

    Update

--- src/test/java/DeliveryServiceTest.java
+++ src/test/java/DeliveryServiceTest.java
@@ -1,138 +1,249 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import java.io.*;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class DeliveryServiceTest {
+public class DeliveryControllerTest {
 	
-    @Test
-    public void testGetDeliveryServiceInstance(){
-        assertNotNull(DeliveryService.getInstance());
-    }
-    
-    @Test
-    public void testUpdateDeliveryPrice() {
-    	DeliveryService.getInstance().updateDeliveryPrice(20, 15);
-    	assertEquals(20,DeliveryService.getInstance().getDeliveryFirstKG_price());
-    	assertEquals(15,DeliveryService.getInstance().getDeliveryAfterFirstKG_price());
-    }
-    
-    @Test
-    public void testRemoveDeliveryZone1() throws DeliveryZoneNotFoundException {
-    	DeliveryService.getInstance().removeDeliveryZone("Kowloon");
-    	assertEquals(new ArrayList<String>(List.of("Hong Kong", "New Territories","Islands District")),DeliveryService.getInstance().getDeliveryZoneList());
-    }
-    
-    @Test
-    public void testRemoveDeliveryZone2(){
-    	Exception e = assertThrows(DeliveryZoneNotFoundException.class,() -> {
-    		DeliveryService.getInstance().removeDeliveryZone("Cheung Chau");
-    	   });
-    	assertEquals("Zone: Cheung Chau is not found.",e.getMessage());
-    }
-    
-    @Test
-    public void testCreateDelivery1() throws DeliveryZoneNotFoundException, DeliveryItemNotFoundException {
-    	Delivery d = DeliveryService.getInstance().createDelivery("9999", "New Territories", "#19 G/F Yau Kom Tau Village, Kowloon,Hongkong", 3.4);
-    	assertEquals(DeliveryService.getInstance().getDelivery("10001"),d);
-    }
-    
-    @Test
-    public void testCreateDelivery2(){
-    	Exception e = assertThrows(DeliveryZoneNotFoundException.class,() -> {
-    		DeliveryService.getInstance().createDelivery("9999", "Tuen Mun", "#19 G/F Yau Kom Tau Village, Kowloon,Hongkong", 3.4);
-    	   });
-    	assertEquals("Zone: Tuen Mun is not found.",e.getMessage());
-    }
-    
-    @Test
-    public void testGetDelivery(){
-    	Exception e = assertThrows(DeliveryItemNotFoundException.class,() -> {
-    		DeliveryService.getInstance().getDelivery("12345");
-    	   });
-    	assertEquals("Delivery Item: 12345 is not found.",e.getMessage());
-    }
+	@Test
+	public void entryDeliveryViewTest() {
+	    System.setIn(new ByteArrayInputStream("-1".getBytes()));
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outContent));
+		DeliveryController dc = new DeliveryController();
+		dc.entryDeliveryView();
+	    assertTrue(outContent.toString().contains("Invalid choice!"));
+	}
+	
+	@Test
+	public void entryDeliveryViewTest1() {
+	    System.setIn(new ByteArrayInputStream("1\n10001\n999\nKowloon\nCityU AC2\n6.34\n8".getBytes()));
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outContent));
+		DeliveryController dc = new DeliveryController();
+		dc.entryDeliveryView();
+		String result = outContent.toString();
+		String expect = "Please enter your choice:";
+		assertTrue(result.contains(expect));
+	}
+	
+	@Test
+	public void entryDeliveryViewTest2() throws Exception {
+		DeliveryService.getInstance().createDelivery("13213", "Kowloon", "fsdfsdf", 0.3);
+	    System.setIn(new ByteArrayInputStream("2\n10001\n8\n".getBytes()));
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outContent));
+		DeliveryController dc = new DeliveryController();
+		dc.entryDeliveryView();
+		String result = outContent.toString();
+		System.err.print(outContent.toString());
+		String expect = "Please enter your choice:";
+		assertTrue(result.contains(expect));
+	}
+	
+	@Test
+	public void entryDeliveryViewTest3() throws Exception {
+	    System.setIn(new ByteArrayInputStream("2\n10019\n8\n".getBytes()));
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outContent));
+		DeliveryController dc = new DeliveryController();
+		dc.entryDeliveryView();
+		String result = outContent.toString();
+		System.err.print(outContent.toString());
+		String expect = "Please enter your choice:";
+		assertTrue(result.contains(expect));
+	}
+	
+	@Test
+	public void entryDeliveryViewTest4() throws Exception {
+	    System.setIn(new ByteArrayInputStream("3\n11\n9\n8\n".getBytes()));
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outContent));
+		DeliveryController dc = new DeliveryController();
+		dc.entryDeliveryView();
+		String result = outContent.toString();
+		System.err.print(outContent.toString());
+		String expect = "Please enter your choice:";
+		assertTrue(result.contains(expect));
+	}
+	
+	@Test
+	public void entryDeliveryViewTest5() throws Exception {
+	    System.setIn(new ByteArrayInputStream("4\n10003\n8\n".getBytes()));
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outContent));
+		DeliveryController dc = new DeliveryController();
+		dc.entryDeliveryView();
+		String result = outContent.toString();
+		System.err.print(outContent.toString());
+		String expect = "Please enter your choice:";
+		assertTrue(result.contains(expect));
+	}
+	
+	@Test
+	public void entryDeliveryViewTest6() throws Exception {
+	    System.setIn(new ByteArrayInputStream("5\n8\n".getBytes()));
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outContent));
+		DeliveryController dc = new DeliveryController();
+		dc.entryDeliveryView();
+		String result = outContent.toString();
+		System.err.print(outContent.toString());
+		String expect = "Please enter your choice:";
+		assertTrue(result.contains(expect));
+	}
+	
+	@Test
+	public void entryDeliveryViewTest7() throws Exception {
+	    System.setIn(new ByteArrayInputStream("6\nHam Tin\n8\n".getBytes()));
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outContent));
+		DeliveryController dc = new DeliveryController();
+		dc.entryDeliveryView();
+		String result = outContent.toString();
+		System.err.print(outContent.toString());
+		String expect = "Please enter your choice:";
+		assertTrue(result.contains(expect));
+	}
+	
+	@Test
+	public void entryDeliveryViewTest8() throws Exception {
+	    System.setIn(new ByteArrayInputStream("7\nHam Tin\n8\n".getBytes()));
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outContent));
+		DeliveryController dc = new DeliveryController();
+		dc.entryDeliveryView();
+		String result = outContent.toString();
+		System.err.print(outContent.toString());
+		String expect = "Please enter your choice:";
+		assertTrue(result.contains(expect));
+	}
+	
+	@Test
+	public void editDeliveryInfoTest() throws DeliveryZoneNotFoundException {
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outContent));
+		DeliveryService.getInstance().addDeliveryZone("Kowloon");
+		DeliveryService.getInstance().createDelivery("10001", "Kowloon", "Cityu", 5.2);
+	    DeliveryController dc = new DeliveryController();
+	    dc.editDeliveryInfo("10001", "9999", "Kowloon", "Cityu", 6.9);
+	    assertEquals("Success !\n", outContent.toString());
+	}
+	@Test
+	public void editDeliveryInfoTest1() throws DeliveryZoneNotFoundException {
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outContent));
+		DeliveryService.getInstance().createDelivery("1999", "Kowloon", "Cityu", 5.2);
+	    DeliveryController dc = new DeliveryController();
+	    dc.editDeliveryInfo("11155", "9999", "Kowloon", "Cityu", 6.9);
+	    assertEquals("DeliveryItemNotFoundException: Delivery Item: 11155 is not found.\n", outContent.toString());
+	}
+	
+	@Test
+	public void editDeliveryInfoTest2() throws DeliveryZoneNotFoundException {
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outContent));
+		DeliveryService.getInstance().createDelivery("10008", "Kowloon", "Cityu", 5.2);
+	    DeliveryController dc = new DeliveryController();
+	    dc.editDeliveryInfo("10001", "9999", "Shanghai", "Cityu", 6.9);
+	    assertEquals("DeliveryZoneNotFoundException: Zone: Shanghai is not found.\n", outContent.toString());
+	}
+	
+	@Test
+	public void editDeliverySystemPriceTest() {
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outContent));
+		DeliveryController dc = new DeliveryController();
+		dc.editDeliverySystemPrice(99, 9999);
+	    assertEquals("Success !\n", outContent.toString());
+	}
 
-    
-    @Test
-    public void testUpdateDelivery() throws DeliveryZoneNotFoundException{
-    	DeliveryService.getInstance().createDelivery("9999", "New Territories", "#19 G/F Yau Kom Tau Village, Kowloon,Hongkong", 3.4);
-    	Exception e = assertThrows(DeliveryZoneNotFoundException.class,() -> {
-        	DeliveryService.getInstance().updateDelivery("10001", "9999", "Greater Bay Area", "Cheung's Ancestral Hall, Tai Om Road", 6.9);
-    	   });
-    	assertEquals("Zone: Greater Bay Area is not found.",e.getMessage());
-    }
-    
-    @Test
-    public void testUpdateDelivery2() throws DeliveryZoneNotFoundException{
-    	DeliveryService.getInstance().createDelivery("9999", "New Territories", "#19 G/F Yau Kom Tau Village, Kowloon,Hongkong", 3.4);
-    	Exception e = assertThrows(DeliveryItemNotFoundException.class,() -> {
-        	DeliveryService.getInstance().updateDelivery("420", "9999", "New Territories", "Cheung's Ancestral Hall, Tai Om Road", 6.9);
-    	   });
-    	assertEquals("Delivery Item: 420 is not found.",e.getMessage());
-    }
-    
-    @Test
-    public void testUpdateDelivery3() throws DeliveryZoneNotFoundException, DeliveryItemNotFoundException{
-    	DeliveryService.getInstance().createDelivery("9999", "New Territories", "#19 G/F Yau Kom Tau Village, Kowloon,Hongkong", 3.4);
-    	assertNotNull(DeliveryService.getInstance().getDelivery("10001"));
-    }
-    
-    @Test
-    public void testUpdateDelivery4() throws DeliveryZoneNotFoundException, DeliveryItemNotFoundException{
-    	DeliveryService.getInstance().createDelivery("9999", "New Territories", "#19 G/F Yau Kom Tau Village, Kowloon,Hongkong", 3.4);
-        DeliveryService.getInstance().updateDelivery("10001", "9999", "Hong Kong", "Cheung's Ancestral Hall, Tai Om Road", 6.9);
-    	assertNotNull(DeliveryService.getInstance().getDelivery("10001"));
-    }
-    
-    @Test
-    public void testDeleteDelivery1() throws DeliveryItemNotFoundException, DeliveryZoneNotFoundException{
-    	DeliveryService.getInstance().createDelivery("9999", "New Territories", "#19 G/F Yau Kom Tau Village, Kowloon,Hongkong", 3.4);
-    	DeliveryService.getInstance().createDelivery("9999", "New Territories", "#19 G/F Yau Kom Tau Village, Kowloon,Hongkong", 3.4);
-    	DeliveryService.getInstance().deleteDelivery("10002");
-    	Exception e = assertThrows(DeliveryItemNotFoundException.class,() -> {
-        	DeliveryService.getInstance().getDelivery("10002");
-    	   });
-    	assertEquals("Delivery Item: 10002 is not found.",e.getMessage());
-    }
-    
-    @Test
-    public void testDeleteDelivery2() throws DeliveryItemNotFoundException, DeliveryZoneNotFoundException{
-    	DeliveryService.getInstance().createDelivery("9999", "New Territories", "#19 G/F Yau Kom Tau Village, Kowloon,Hongkong", 3.4);
-    	DeliveryService.getInstance().createDelivery("9999", "New Territories", "#19 G/F Yau Kom Tau Village, Kowloon,Hongkong", 3.4);
-    	Exception e = assertThrows(DeliveryItemNotFoundException.class,() -> {
-        	DeliveryService.getInstance().deleteDelivery("10015");
-    	   });
-    	assertEquals("Delivery Item: 10015 is not found.",e.getMessage());
-    }
-    
-    @Test
-    public void testCalculateDeliveryPrice1() {
-    	DeliveryService.getInstance().updateDeliveryPrice(25, 10);
-    	assertEquals(105, DeliveryService.getInstance().calculateDeliveryPrice(2.5, "Island District"));
-    }
+	@Test
+	public void findDeliveryTest()  throws DeliveryZoneNotFoundException, DeliveryItemNotFoundException {
+		DeliveryService.getInstance().createDelivery("100044", "Islands District", "Cityu", 5.2);
+	    DeliveryController dc = new DeliveryController();
+	    assertEquals(DeliveryService.getInstance().getDelivery("10001"),dc.findDelivery("10001"));
+	}
+	
+	@Test
+	public void findDeliveryTest1()  throws DeliveryZoneNotFoundException, DeliveryItemNotFoundException {
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outContent));
+		DeliveryService.getInstance().createDelivery("100044", "New Territories", "Cityu", 5.2);
+	    DeliveryController dc = new DeliveryController();
+	    assertEquals(null,dc.findDelivery("10110"));
+	}
+	
+	@Test
+	public void createDeliveryTest() {
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outContent));
+	    DeliveryController dc = new DeliveryController();
+	    dc.createDelivery("1000", "Hong Kong", "CityU", 0.3);
+	    assertEquals("Success !\n", outContent.toString());
+	}
+	
+	@Test
+	public void createDeliveryTest1() {
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outContent));
+	    DeliveryController dc = new DeliveryController();
+	    dc.createDelivery("1000", "Ting Kau", "CityU", 0.3);
+	    assertEquals("DeliveryZoneNotFoundException: Zone: Ting Kau is not found.\n", outContent.toString());
+	}
+	
+	@Test
+	public void addDeliveryZoneTest() {
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outContent));
+	    DeliveryController dc = new DeliveryController();
+	    dc.addDeliveryZone("Sai Kung");
+	    assertEquals("Success !\n", outContent.toString());
+	}
+	
+	@Test
+	public void deleteDeliveryZoneTest() {
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outContent));
+	    DeliveryController dc = new DeliveryController();
+	    DeliveryService.getInstance().addDeliveryZone("Sai Kung");
+	    dc.deleteDeliveryZone("Sai Kung");
+	    assertEquals("Success !\n", outContent.toString());
+	}
+	
+	@Test
+	public void deleteDeliveryZoneTest1() {
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outContent));
+	    DeliveryController dc = new DeliveryController();
+	    dc.deleteDeliveryZone("Hong Kong1");
+	    assertEquals("DeliveryZoneNotFoundException: Zone: Hong Kong1 is not found.\n", outContent.toString());
+	}
+	
+	@Test
+	public void deleteDeliveryTest() throws Exception{
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outContent));
+	    DeliveryService.getInstance().createDelivery("10008", "Kowloon", "Cityu", 5.2);
+	    DeliveryController dc = new DeliveryController();		
+	    dc.deleteDelivery("10004");
+	    assertEquals("Success !\n", outContent.toString());
+	}
+	
+	@Test
+	public void deleteDeliveryTest1() throws Exception{
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outContent));
+	    DeliveryService.getInstance().createDelivery("10008", "Kowloon", "Cityu", 5.2);
+	    DeliveryController dc = new DeliveryController();		
+	    dc.deleteDelivery("11155");
+	    assertEquals("DeliveryItemNotFoundException: Delivery Item: 11155 is not found.\n", outContent.toString());
+	}
+	
+	@Test
+	public void getDeliveryListTest() throws Exception {
+		DeliveryController dc = new DeliveryController();		
+	    DeliveryService.getInstance().createDelivery("10008", "Kowloon", "Cityu", 5.2);
+		assertNotNull(dc.getDeliveryList());
+	}
 
-    @Test
-    public void testCalculateDeliveryPrice2() {
-    	DeliveryService.getInstance().updateDeliveryPrice(25, 10);
-    	assertEquals(85, DeliveryService.getInstance().calculateDeliveryPrice(6.9, "Kowloon"));
-    }
-    @Test
-    public void testCalculateDeliveryPrice3() {
-    	DeliveryService.getInstance().updateDeliveryPrice(25, 10);
-    	assertEquals(95, DeliveryService.getInstance().calculateDeliveryPrice(6.9, "Hong Kong"));
-    }
-    @Test
-    public void testGetDeliveryList() throws DeliveryZoneNotFoundException {
-    	DeliveryService.getInstance().createDelivery("9999", "New Territories", "#19 G/F Yau Kom Tau Village, Kowloon,Hongkong", 3.4);
-    	assertNotNull(DeliveryService.getInstance().getDeliveryList());
-    }
-    
-    @Test
-    public void testAddDeliveryZone() {
-    	DeliveryService.getInstance().addDeliveryZone("Lok Ma Chau");
-    	assertNotNull(DeliveryService.getInstance().getDeliveryZoneList());
-    }
- 
 }
